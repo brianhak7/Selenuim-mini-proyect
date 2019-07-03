17b07396476d8f2b1142e29ea4af06498dacbfc0
brianhak7 <colonb70@gmail.com>
2019-07-03 11:45:52 -0400

    Add files via upload

---
+++ TwitterTest/Loginerror.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests_Framework;
+
+namespace TwitterTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class Loginerror
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+
+        }
+        
+
+
+
+        [TestMethod]
+        public void testerror()
+        {
+            LoginPage loginPage = new LoginPage();
+          //  loginPage.GoTo();
+         //   LoginPage.LoginAs(Password.UserName).WithPassword("hjklesadadf").Login();
+            Assert.IsTrue(Logincheck.IsAt,"Login error");
+        }
+    }
+}
--- TwitterTest/loginfb.cs
+++ TwitterTest/loginfb.cs
@@ -1,24 +1,26 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tests_Framework;
 
 namespace TwitterTest
 {
     [TestClass]
-    public class TwitterTest
+    public class loginfb
     {
         [TestInitialize]
         public void Init()
         {
-          Driver.Initialize();
+            Driver.Initialize();
 
         }
         [TestMethod]
         public void TestMethod1()
         {
             LoginPage loginPage = new LoginPage();
-           // loginPage.GoTo("https://www.facebook.com/");
-        //    LoginPage.LoginAs(Password.UserNamefb).WithPassword(Password.Passw0rdfb).Login();
+            loginPage.GoTo("https://www.facebook.com/", "inputtext _55r1 inputtext _1kbt inputtext _1kbt");
+            LoginPage.LoginAs(Password.UserNamefb).WithPassword(Password.Passw0rdfb).Login("email","pass", "loginbutton","fb");
         }
     }
 }
