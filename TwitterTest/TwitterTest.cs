3bfc673e723a00815f60738bebf10805c58b0b76
brianhak7 <colonb70@gmail.com>
2019-07-03 15:26:09 -0400

    Add files via upload

--- TwitterTest/TwitterTest.cs
+++ TwitterTest/TwitterTest.cs
@@ -5,7 +5,7 @@ using Tests_Framework;
 namespace TwitterTest
 {
     [TestClass]
-    public class TwitterTest
+    public class FBLogin
     {
         [TestInitialize]
         public void Init()
@@ -16,9 +16,10 @@ namespace TwitterTest
         [TestMethod]
         public void TestMethod1()
         {
+
             LoginPage loginPage = new LoginPage();
-           // loginPage.GoTo("https://www.facebook.com/");
-        //    LoginPage.LoginAs(Password.UserNamefb).WithPassword(Password.Passw0rdfb).Login();
+            loginPage.GoTo("https://twitter.com/", "js-username-field email-input js-initial-focus");
+            LoginPage.LoginAs(Password.UserNamefb).WithPassword(Password.Passw0rdfb).Login("js-username-field", "js-password-field", "submit","tw");
         }
     }
 }
